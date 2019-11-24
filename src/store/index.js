29ba300d2fe85790c9137aeb9b1211971cfe94d5
EmmanuelYupit <emmanuelyupit08@gmail.com>
2019-11-24 03:05:15 -0500

    project set up

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleWare from 'redux-saga'
+import rootReducer from '../redux/reducers'
+import rootSaga from '../redux/sagas'
+import logger from '../middleware/logger'
+
+const sagaMiddleware = createSagaMiddleWare()
+const middlewares = [logger, sagaMiddleware]
+
+const appliedMiddlewares = applyMiddleware(...middlewares)
+
+const store = createStore(rootReducer, compose(appliedMiddlewares))
+
+sagaMiddleware.run(rootSaga)
+
+export default store
