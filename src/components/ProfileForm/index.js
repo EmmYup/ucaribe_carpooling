718e33d64f188d45a8da3061ee6ae702e7917ea1
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-12-04 13:05:40 -0500

    avatar in profile fixed

--- src/components/ProfileForm/index.js
+++ src/components/ProfileForm/index.js
@@ -10,7 +10,7 @@ import {
     IonButton,
 } from '@ionic/react';
 import { create } from 'ionicons/icons';
-import { Wrap, Avatar } from './index.style';
+import { Wrap, AvatarContainer, Avatar } from './index.style';
 
 class ProfileForm extends Component {
     constructor(props) {
@@ -40,11 +40,11 @@ class ProfileForm extends Component {
         const { type = 'student', carModel, licensePlate, color } = this.props;
         return (
             <>
-                <Avatar>
-                    <IonAvatar>
+                <AvatarContainer>
+                    <Avatar>
                         <IonImg src="https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/p960x960/39165088_707855179556004_3115956180057325568_o.jpg?_nc_cat=100&_nc_ohc=9vbI8F-iOecAQn8XgxtmTcifwD7oFqJjgIQPvmmn5tWmlldqnSdx4nGuw&_nc_ht=scontent.fmid3-1.fna&oh=2a6da6a2d95c00ef15671d9305cfc96a&oe=5E80DCF9" />
-                    </IonAvatar>
-                </Avatar>
+                    </Avatar>
+                </AvatarContainer>
                 <Wrap>
                     <IonItem>
                         <IonInput
--- src/components/ProfileForm/index.style.js
+++ src/components/ProfileForm/index.style.js
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
+import { IonAvatar } from '@ionic/react';
 
 export const Wrap = styled.div`
     position: absolute;
     left: 5%;
-    top: 30%;
+    top: 32%;
     width: 90%;
 `;
 
-export const Avatar = styled.div`
+export const AvatarContainer = styled.div`
     display: flex;
     justify-content: center;
     /* margin-top: 10%; */
+`;
+
+export const Avatar = styled(IonAvatar)`
+    width: 40%;
+    height: 40%;
 `;
